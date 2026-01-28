b00a1ffe48510c1438dae81b24dabc7942c9ce32
minhaj <minhajahmd0021@gmail.com>
2026-01-28 18:59:25 +0530

    final

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -23,10 +23,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@%mie3j-p2&=qs=g&c^i%#t_^riz)e7(fb64k0=os3ha39cev_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
