99995ad8d236642157d15b4f1acd0adfc59fc3e5
minhaj <minhajahmd0021@gmail.com>
2026-01-28 19:58:20 +0530

    final

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@%mie3j-p2&=qs=g&c^i%#t_^riz)e7(fb64k0=os3ha39cev_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
